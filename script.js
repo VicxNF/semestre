d6742e64ee33ef5a22eaf4d252f5fa7477d29762
vicxnf <vic.navarro@duocuc.cl>
2022-05-02 22:30:57 -0400

    carrito script avance

--- script.js
+++ script.js
@@ -30,3 +30,86 @@ function resetActiveImg(){
         img.parentElement.classList.remove('active');
     });
 }
+
+let carts = document.querySelectorAll('.add-cart-btn');
+
+let products = [
+    {
+        name: 'Margarita',
+        tag: 'margarita',
+        price: 2990,
+        inCart: 0
+    },
+    {
+        name: 'Azalea',
+        tag: 'azalea',
+        price: 2990,
+        inCart: 0
+    },
+    {
+        name: 'Hortensia',
+        tag: 'hortensia',
+        price: 2990,
+        inCart: 0
+    },
+    {
+        name: 'Lirio Rosado',
+        tag: 'lirio',
+        price: 2990,
+        inCart: 0
+    }
+];
+
+for (let i=0; i < carts.length; i++){
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+    })
+}
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers){
+        document.querySelector('.cart-btn span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart-btn span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart-btn span').textContent = 1;
+    }
+
+    setItems(product);
+}
+
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag] : product
+        }
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+onLoadCartNumbers();
